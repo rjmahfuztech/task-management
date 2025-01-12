374525d8bab9661ab55403be46f4fa304e18797d
rjmahfuztech <rjmahfuztech@gmail.com>
2025-01-12 14:00:55 +0600

    django templates added with dashboard

--- tasks/views.py
+++ tasks/views.py
@@ -1,20 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def home(request):
-    return HttpResponse("This is My first Django APP!")
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
 
-def contact(request):
-    return HttpResponse("<h1 style='text-align: center; color:red';>Hello World!!</h1>")
-
-def defaultHome(request):
-    return HttpResponse("Default Home Page")
-
-def show_task(request):
-    return HttpResponse("This is Show task page!")
-
-def show_specific_task(request, id):
-    print("id ",id)
-    print("id type ",type(id))
-    return HttpResponse(f"this is Specific task page! id: {id}")
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
